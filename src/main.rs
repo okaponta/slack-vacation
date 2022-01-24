1e4ebec71457fe6aeae3abe0ad6a614f83793aa9
Kohei Okamoto <okaponta.19910919@gmail.com>
2022-01-24 22:58:43 +0900

    refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chrono::{Duration, Local};
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 use regex::Regex;
-use reqwest::header;
+use reqwest::header::{self, HeaderMap};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -19,11 +19,11 @@ struct SlackResponse {
 
 #[tokio::main]
 async fn main() {
-    run().await;
+    run(app_info()).await;
 }
 
-async fn run() {
-    let matches = App::new("slack-vacation")
+fn app_info() -> ArgMatches {
+    App::new("slack-vacation")
         .version("1.0.0")
         .author("@okaponta")
         .about("reveals you're in vacation in slack!!")
@@ -50,10 +50,11 @@ async fn run() {
                 .long("date")
                 .required(false),
         )
-        .get_matches();
+        .get_matches()
+}
 
+async fn run(matches: ArgMatches) {
     if let Some(token) = matches.value_of("token") {
-        println!("token specified: {}", token);
         if matches.is_present("back") {
             back_from_vacation(token).await;
         } else {
@@ -80,90 +81,31 @@ const GET_URI: &str = "https://slack.com/api/users.profile.get";
 async fn go_to_vacation(token: &str, date: &str) {
     println!("I'm going to vacation: {} {}", token, date);
 
-    let prev = get_username(token).await;
-    let next = prev + "(" + date + "休)";
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_str("application/json; charset=utf-8").unwrap(),
-    );
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
-    );
-    headers.insert(
-        header::AUTHORIZATION,
-        header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
-    );
-
-    let response = reqwest::ClientBuilder::new()
-        .default_headers(headers)
-        .build()
-        .unwrap()
-        .post(SET_URI)
-        .query(&[("name", "display_name"), ("value", &next)])
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
-
-    println!("{}", response);
+    let next = add_vacation(&get_username(token).await, date);
+    set_slack_username(token, &next).await;
 }
 
 async fn back_from_vacation(token: &str) {
     println!("I'm back from vacation: {}", token);
-    let prev = get_username(token).await;
-    let re = Regex::new(r"\(.*休\)").unwrap();
-    let next = re.replace_all(&prev, "").to_string();
-    println!("{}", next);
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_str("application/json; charset=utf-8").unwrap(),
-    );
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
-    );
-    headers.insert(
-        header::AUTHORIZATION,
-        header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
-    );
 
-    let response = reqwest::ClientBuilder::new()
-        .default_headers(headers)
-        .build()
-        .unwrap()
-        .post(SET_URI)
-        .query(&[("name", "display_name"), ("value", &next)])
-        .send()
-        .await
-        .unwrap()
-        .text()
-        .await
-        .unwrap();
+    let next = remove_vacation(&get_username(token).await);
+    set_slack_username(token, &next).await;
+}
 
-    println!("{}", response);
+fn add_vacation(prev: &str, date: &str) -> String {
+    prev.to_string() + "(" + date + "休)"
+}
+
+fn remove_vacation(prev: &str) -> String {
+    Regex::new(r"\(.*休\)")
+        .unwrap()
+        .replace_all(&prev, "")
+        .to_string()
 }
 
 async fn get_username(token: &str) -> String {
-    println!("I'm going to vacation: {}", token);
-
-    let mut headers = header::HeaderMap::new();
-    headers.insert(
-        header::CONTENT_TYPE,
-        header::HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
-    );
-    headers.insert(
-        header::AUTHORIZATION,
-        header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
-    );
-
     let response = reqwest::ClientBuilder::new()
-        .default_headers(headers)
+        .default_headers(get_headers(token))
         .build()
         .unwrap()
         .get(GET_URI)
@@ -174,10 +116,70 @@ async fn get_username(token: &str) -> String {
         .await
         .unwrap();
 
-    println!("{}", response);
-
     let response_struct: SlackResponse = serde_json::from_str(&response).unwrap();
-    let disp_name = response_struct.profile.unwrap().display_name;
+    response_struct.profile.unwrap().display_name
+}
 
-    disp_name
+async fn set_slack_username(token: &str, next: &str) {
+    reqwest::ClientBuilder::new()
+        .default_headers(post_headers(token))
+        .build()
+        .unwrap()
+        .post(SET_URI)
+        .query(&[("name", "display_name"), ("value", &next)])
+        .send()
+        .await
+        .unwrap();
+}
+
+fn get_headers(token: &str) -> HeaderMap {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
+    );
+    headers.insert(
+        header::AUTHORIZATION,
+        header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
+    );
+    headers
+}
+
+fn post_headers(token: &str) -> HeaderMap {
+    let mut headers = header::HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_str("application/json; charset=utf-8").unwrap(),
+    );
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_str("application/x-www-form-urlencoded").unwrap(),
+    );
+    headers.insert(
+        header::AUTHORIZATION,
+        header::HeaderValue::from_str(&format!("Bearer {}", token)).unwrap(),
+    );
+    headers
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_vacation() {
+        let before = "hoge";
+        let date = "01/23";
+        let after = add_vacation(before, date);
+
+        assert_eq!(after, "hoge(01/23休)");
+    }
+
+    #[test]
+    fn test_remove_vacation() {
+        let before = "hoge(01/23休)";
+        let after = remove_vacation(before);
+
+        assert_eq!(after, "hoge");
+    }
 }
